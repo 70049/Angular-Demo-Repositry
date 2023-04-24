38f23742196ff00b96ed409dc80ef7f6f314e7fb
Rajeev Kumar <rk488296@gmail.com>
2023-04-24 11:15:34 +0530

    Add all mising file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,39 +3,39 @@ import { WeatherService } from './Services/weather.service';
 import { WeatherData } from './models/weather.model';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
+     selector: 'app-root',
+     templateUrl: './app.component.html',
 })
 export class AppComponent implements OnInit {
 
-  constructor(public weatherService: WeatherService) { }
+     constructor(public weatherService: WeatherService) { }
 
-  public weatherData?: WeatherData;
-  public cityName: string = "newyork";
+     public weatherData?: WeatherData;
+     public cityName: string = "newyork";
 
 
-  ngOnInit(): void {
-    this.getWeatherInfo(this.cityName);
-    this.cityName = '';
+     ngOnInit(): void {
+          this.getWeatherInfo(this.cityName);
+          this.cityName = '';
 
-  }
+     }
 
-  public searchFeild = () => {
-    this.getWeatherInfo(this.cityName);
-    this.cityName = '';
-  }
+     public searchFeild = () => {
+          this.getWeatherInfo(this.cityName);
+          this.cityName = '';
+     }
 
-  private getWeatherInfo = (cityName: string) => {
-    this.weatherService.getWeatherData(cityName)
-      .subscribe({
-        next: (res) => {
-          this.weatherData = res;
-          console.log(res);
-        },
-        error: (error) => {
-          console.log(error);
-        }
-      });
-  }
+     private getWeatherInfo = (cityName: string) => {
+          this.weatherService.getWeatherData(cityName)
+               .subscribe({
+                    next: (res) => {
+                         this.weatherData = res;
+                         console.log(res);
+                    },
+                    error: (error) => {
+                         console.log(error);
+                    }
+               });
+     }
 
 }
